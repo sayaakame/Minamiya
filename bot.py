5ee35a8e0233da1e680a0fb57f20dfc0df41d483
sayaakame <52957436+sayaakame@users.noreply.github.com>
2019-07-22 09:40:42 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.ext.commands.cooldowns import BucketType
+import asyncio
+import platform
+import colorsys
+import random
+import os
+import time
+from discord.voice_client import VoiceClient
+from discord import Game, Embed, Color, Status, ChannelType
+
+
+Forbidden= discord.Embed(title="Permission Denied", description="1) Please check whether you have permission to perform this action or not. \n2) Please check whether my role has permission to perform this action in this channel or not. \n3) Please check my role position.", color=0x00ff00)
+client = Bot(description="DarkBot Bot is best", command_prefix="d!", pm_help = True)
+client.remove_command('help')
+
+
+async def status_task():
+    while True:
+        await client.change_presence(game=discord.Game(name='for d!help'))
+        await asyncio.sleep(5)
+        await client.change_presence(game=discord.Game(name='with '+str(len(set(client.get_all_members())))+' users'))
+        await asyncio.sleep(5)
+        await client.change_presence(game=discord.Game(name='in '+str(len(client.servers))+' servers'))
+        await asyncio.sleep(5)
+        
+
+	
+@client.event
+async def on_ready():
+    print('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    print('--------')
+    print('--------')
+    print('Started Dark BOT')
+    print('Created by Utkarsh')
+    client.loop.create_task(status_task())
+    
+@client.event
+async def on_message(message):
+	await client.process_commands(message)
+
+client.run(os.getenv('Token'))
